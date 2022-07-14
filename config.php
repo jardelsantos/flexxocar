6b697f06f1bd725efba247b9db0eb666b2b2e7f1
Jardel Santos <jardel.santos@vonex.com.br>
2022-07-14 19:00:01 -0300

    adição das configurações de banco de dados

--- config.php
+++ config.php
@@ -1,3 +1,8 @@
 <?php
 
 $config['nome_sistema'] = 'FlexxoCar';
+
+$config['database']['servidor'] = 'localhost';
+$config['database']['usuario']  = 'root';
+$config['database']['senha']    = '';
+$config['database']['banco']    = 'flexxocar';
