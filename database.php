afc45cac9a5a1282b6b63143579aedf109856579
Jardel Santos <jardel.santos@vonex.com.br>
2022-07-14 19:08:00 -0300

    Movendo a função de conexão

--- database.php
+++ database.php
@@ -1,7 +1,19 @@
 <?php
-
+/*
 $veiculos = [];
 
 $clientes = [];
 
 $contratos = [];
+*/
+$link = '';
+
+function conexao(){
+    global $link;
+    $link = mysqli_connect(
+        $config['database']['servidor'], 
+        $config['database']['usuario'], 
+        $config['database']['senha'], 
+        $config['database']['banco']
+    );
+}
